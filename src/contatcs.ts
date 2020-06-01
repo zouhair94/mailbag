965bb56e5b34defff98c978ef290b75a4f504dc8
zouhair94 <z.ridaoui94@outlook.com>
2020-06-02 00:44:50 +0300

    feature: add list contacts

--- src/contatcs.ts
+++ src/contatcs.ts
@@ -8,5 +8,22 @@ export interface IContact {
 
 
 export class Worker {
-
+    private db: Nedb;
+    constructor() {
+        this.db = Datastore({
+            filename: path.join(__dirname,"contacts.db"),
+            autoload: true
+        });
+    }
+    public listContacts(): Promise<IContact[]|Error> {
+        return new Promise((rej,res)=>{
+            this.db.find({},(err: Error, docs: IContact[])=>{
+                if(err) {
+                    rej(err);
+                }else {
+                    res(docs)
+                }
+            });
+        });
+    }
 }
